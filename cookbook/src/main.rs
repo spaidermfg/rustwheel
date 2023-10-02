b99b26b794a788235c3cd6879bc2fc43ec3524ea
Toymachine <488434442@qq.com>
2023-10-02 19:53:45 +0800

    random

--- cookbook/src/main.rs
+++ cookbook/src/main.rs
@@ -1,3 +1,9 @@
+use crate::random::rand_num;
+
+mod random;
+
 fn main() {
     println!("Hello, Cookbook!");
+
+    rand_num();
 }
---
+++ cookbook/src/random.rs
@@ -0,0 +1,43 @@
+use rand::distributions::Uniform;
+use rand::distributions::Distribution;
+use rand::Rng;
+
+pub fn rand_num() {
+    println!("------------------------------------rand num");
+    rand1();
+
+    println!("------------------------rand2");
+    rand2();
+}
+
+// 生成随机数
+fn rand1() {
+    let mut rng = rand::thread_rng();
+
+    let n1: u8 = rng.gen();
+    let n2: u16 = rng.gen();
+
+    println!("Random u8: {}", n1);
+    println!("Random u16: {}", n2);
+    println!("Random u32: {}", rng.gen::<u32>());
+    println!("Random i32: {}", rng.gen::<i32>());
+    println!("Random float: {}", rng.gen::<f64>());
+}
+
+// 生成范围内随机数
+fn rand2() {
+    let mut rng = rand::thread_rng();
+
+    println!("Integer: {}", rng.gen_range(0..10));
+    println!("Float: {}", rng.gen_range(0.0..10.0));
+
+    let mut rng1 = rand::thread_rng();
+    let die = Uniform::from(1..40);
+    loop {
+        let throw = die.sample(&mut rng1);
+        println!("Roll the die: {}", throw);
+        if throw == 6 {
+            break;
+        }
+    }
+}
