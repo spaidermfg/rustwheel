3329cbc37bf900a6e60b29d64b9ab28f9e8f9fd8
Toymachine <488434442@qq.com>
2023-12-20 23:04:17 +0800

    grep lite

--- cookbook/src/high.rs
+++ cookbook/src/high.rs
@@ -1,11 +1,12 @@
 use std::fs::{File, read};
+use std::io;
 use std::io::{BufRead, BufReader};
 use std::rc::Rc;
 use std::sync::{Arc, Mutex};
 use num::Complex;
 use std::time::{Instant,Duration};
 use std::ops::Add;
-use regex::Regex;
+use regex::{Match, Regex};
 use clap::{Arg,App};
 
 pub fn higher() {
@@ -298,21 +299,20 @@ fn command() {
         .arg(Arg::with_name("input")
             .help("File to search")
             .takes_value(true)
-            .required(true)).get_matches();
+            .required(false)).get_matches();
 
     let pattern = args.value_of("pattern").unwrap();
     let re = Regex::new(pattern).unwrap();
 
-    let input = args.value_of("input").unwrap();
-    let f = File::open(input).unwrap();
-    let reader = BufReader::new(f);
-
-    for line_ in reader.lines() {
-        let line = line_.unwrap();
-        match re.find(&line) {
-            Some(_) => println!("{}", line),
-            None => (),
-        }
+    let input = args.value_of("input").unwrap_or("-");
+    if input == "-" {
+        let stdin = io::stdin();
+        let reader = stdin.lock();
+        process_lines(reader, re);
+    } else {
+        let f = File::open(input).unwrap();
+        let reader = BufReader::new(f);
+        process_lines(reader, re);
     }
 }
 
@@ -322,5 +322,15 @@ fn read_file() {
     for line_ in reader.lines() {
         let line = line_.unwrap();
         println!("{}-{} bytes long", line, line.len());
+    }
+}
+
+fn process_lines<T: BufRead + Sized>(reader: T, re: Regex) {
+    for line_ in reader.lines() {
+        let line = line_.unwrap();
+        match re.find(&line) {
+            Some(_) => println!("find: {}", line),
+            None => println!("Is null input"),
+        }
     }
 }
