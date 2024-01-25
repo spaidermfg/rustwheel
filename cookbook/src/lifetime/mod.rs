07e8f7ae1aed49bf79b80466c104e7d01b4acf62
Toymachine <488434442@qq.com>
2024-01-25 22:39:40 +0800

    err: value used here after move

--- cookbook/src/lifetime/mod.rs
+++ cookbook/src/lifetime/mod.rs
@@ -2,9 +2,14 @@
 #![allow(unused_variables)]
 pub fn life_time() {
     println!("{}", "---".repeat(12));
-    let sat_a = 1;
-    let sat_b = 2;
-    let sat_c = 3;
+    let sat_a = CubeSat::new(1);
+    let sat_b = CubeSat::new(2);
+    let sat_c = CubeSat::new(3);
+
+    let a_status = check_status(sat_a);
+    let b_status = check_status(sat_b);
+    let c_status = check_status(sat_c);
+    println!("{:?} {:?} {:?}", a_status, b_status, c_status);
 
     let a_status = check_status(sat_a);
     let b_status = check_status(sat_b);
@@ -12,13 +17,22 @@ pub fn life_time() {
     println!("{:?} {:?} {:?}", a_status, b_status, c_status);
 }
 
+#[derive(Debug)]
+struct CubeSat {
+    id: u64,
+}
 
+impl CubeSat {
+    fn new(sat_id: u64) -> CubeSat {
+        CubeSat{ id: sat_id}
+    }
+}
 
 #[derive(Debug)]
 enum StatusMessage {
     Ok,
 }
 
-fn check_status(sat_id: i64) -> StatusMessage {
+fn check_status(sat_id: CubeSat) -> StatusMessage {
     StatusMessage::Ok
 }
