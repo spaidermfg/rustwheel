8769c73e8e382c4a38d07755da68e2c02a64c1ef
Toymachine <488434442@qq.com>
2024-01-25 22:25:03 +0800

    lifetime

--- cookbook/src/lifetime/mod.rs
+++ cookbook/src/lifetime/mod.rs
@@ -1,4 +1,24 @@
 //! lifetime,ownership，borrow
+#![allow(unused_variables)]
 pub fn life_time() {
+    println!("{}", "---".repeat(12));
+    let sat_a = 1;
+    let sat_b = 2;
+    let sat_c = 3;
 
+    let a_status = check_status(sat_a);
+    let b_status = check_status(sat_b);
+    let c_status = check_status(sat_c);
+    println!("{:?} {:?} {:?}", a_status, b_status, c_status);
+}
+
+
+
+#[derive(Debug)]
+enum StatusMessage {
+    Ok,
+}
+
+fn check_status(sat_id: i64) -> StatusMessage {
+    StatusMessage::Ok
 }
