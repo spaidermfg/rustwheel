2bf5750f4557821636501f20fa36b78f74e696c4
Toymachine <488434442@qq.com>
2024-03-01 15:12:57 +0800

    参数使用借用类型

--- codestyle/src/main.rs
+++ codestyle/src/main.rs
@@ -2,4 +2,32 @@
 //! 参考：https://chuxiuhong.com/chuxiuhong-rust-patterns-zh/intro.html
 fn main() {
     println!("Hello, world!");
+    let curious = "curious";
+    println!("{}", three_vowels(curious));
+
+    let sentence_string = "Once upon a time, there was a friendly curious crab named Ferris";
+    for word in sentence_string.split(' ') {
+        println!("{} has three consecutive vowels!", word);
+    }
+}
+
+/// 1. 参数最好使用借用类型
+/// &str > &String
+/// &[T] > &Vec<T>
+/// &T > &Box<T>
+
+fn three_vowels(words: &str) -> bool {
+    let mut vomel_count = 0;
+    for index in words.chars() {
+        match  index {
+            'a' | 'e' | 'i' | 'o' | 'u' => {
+                vomel_count += 1;
+                if vomel_count >= 3 {
+                    return true;
+                }
+            }
+            _ => vomel_count = 0
+        }
+    }
+    false
 }
